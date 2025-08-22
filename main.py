f59e1ff9a1a5d2836a59c3d7c778e6f2e6a3fa8a
010190 <olczakmichal4@gmail.com>
2025-08-22 22:04:43 +0200

    working version with imposing watermark onto selected image

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from tkinter import *
 from tkinter import filedialog, font, colorchooser
 from tkinter.messagebox import askquestion
 from tkinter.simpledialog import askstring, askinteger
-from tkinter.ttk import Combobox
+from tkinter.ttk import Combobox, Style
 from matplotlib import colors
 
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
+# style = Style()
+#
+# style.configure('TButton', font =
+#                ('calibri', 10, 'bold', 'underline'),
+#                 foreground = "#6C85D5")
+
 app = Tk()
 app.title("Watermark application")
-
+app.geometry("400x400")
 fonts = list(font.families())
 
 font_size = [num for num in range(1,50)]
@@ -17,27 +23,35 @@ font_size = [num for num in range(1,50)]
 var_font = StringVar()
 var_size = StringVar()
 
+background = Image.open("C:/Users/olcza/Desktop/watermark_app/Gv56y4SWQAAHLUL.jpg")
+background = background.resize((400, 400))
+background_image = ImageTk.PhotoImage(background)
+
+canvas1 = Canvas( app, width = 400,
+                 height = 400,
+                  borderwidth=0)
+
+canvas1.place(x=-1, y=0)
+# Display image
+canvas1.create_image( 0, 0, image = background_image, anchor = "nw")
 
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir="/Desktop",
                                           title="Select a File",
-                                          filetypes=(("images", "*.jpg"
-                                                      "*.jpeg"),
-                                                     ("all files",
-                                                      "*.*")))
-
-
+                                          filetypes=[('Images','*.jpg *.jpeg *.png')])
     if filename:
 
-        dialog = Toplevel()
-        im = Image.open(filename)
+        dialog = Toplevel(bg="#6C85D5")
+        dialog.geometry("300x300")
+
+        im =  Image.open(open(filename, 'rb'))
 
         im = im.resize((200,200))
-
+        im = im.convert('RGBA')
         render = ImageTk.PhotoImage(im)
         my_var = Label(dialog, image=render,width=200, height=200)
         my_var.image = render
-        my_var.grid(row=1, column=1)
+        my_var.place(x=0, y=0)
 
 
         def text_watermark():
@@ -58,61 +72,58 @@ def browseFiles():
             edit.text((x,y), text=text, fill=color_code, font=font)
             new_im = ImageTk.PhotoImage(im)
             my_var.configure(image=new_im)
-            my_var.grid(row=1, column=1)
+            my_var.place(x=0, y=0)
             my_var.image = new_im
 
             save = askquestion(title="Do you want to save this image?",message="Would you like to save this image?")
             if save == "yes":
                 im.save(f"{text}.jpg")
 
-        # def add_symbol():
-        #     pass
-        #
-        # def save():
-        #     global new_im
-        #     global text
 
+        def photo_watermark():
+            print(filename)
+            watermark_filename = filedialog.askopenfilename(initialdir="/Desktop",
+                                                  title="Select a File",
+                                                  filetypes=[('Images', '*.jpg *.jpeg *.png')])
+            watermark_image = Image.open(open(watermark_filename, 'rb'))
+            im2 = watermark_image.resize((40,40))
+            layer = Image.new('RGBA', im.size, (0, 0, 0, 0))
+            layer.paste(im2, (20, 20))
+            layer2 = layer.copy()
+            layer2.putalpha(1)
+            layer.paste(layer2, (0, 0), layer2)
+            result = Image.alpha_composite(im, layer)
+            result.show()
+            result.save("watermark1.png")
 
-        choose_font = Combobox(dialog, values=fonts, textvariable=var_font,)
-
-
-        add_text = Button(dialog, text="Add text", command=text_watermark)
-        add_text.grid(row=2, column=1, sticky=E)
-        add_sym = Button(dialog, text="Add symbol")
-        add_sym.grid(row=2, column=1, sticky=W)
-
-
-        # save_button = Button(dialog, text="Save", command=save)
-        # save_button.grid(row=2, column=2, sticky=E)
-
-        choose_font.grid(row=3, column=1, sticky=W)
-
-
+        add_text = Button(dialog, text="Add text", command=text_watermark,  style = 'TButton')
+        add_text.place(x=220, y=50)
+        add_sym = Button(dialog, text="Add photo", command=photo_watermark)
+        add_sym.place(x=220, y=80)
+        button_exit = Button(dialog,
+                             text="Exit",
+                             command=exit)
+        button_exit.place(x=220, y=120)
 
 content = Frame(app)
+welcome_label = Label(app, text="Watermark application", font=("Arial", 30), fg="white",  justify="center", bg="#6C85D5")
 
-welcome = Label(app, text="Welcome to Watermark application")
-welcome.grid(column=0, row=0)
-
-
-
-
-label_file_explorer = Label(app,
-                            text = "File Explorer using Tkinter",
-                            width = 100, height = 4,
-                            fg = "blue")
-label_file_explorer.grid(column=1, row=1)
-
+label_canvas = canvas1.create_window(200, 70, window=welcome_label)
 button_explore = Button(app,
                         text="Browse Files",
                         command=browseFiles)
 
-button_explore.grid(column=1, row=2)
+button1_canvas = canvas1.create_window( 200, 300,
+                                       anchor = "center",
+                                       window = button_explore)
 button_exit = Button(app,
                      text="Exit",
                      command=exit)
+button2_canvas = canvas1.create_window( 200, 320,
+                                       anchor = "n",
+                                       window = button_exit)
 
-button_exit.grid(column=1, row=3)
+
 
 
 
